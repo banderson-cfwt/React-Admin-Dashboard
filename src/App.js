b23f991da319d79c0c100214b275517aceb1eb4e
Bryan Anderson <bryan.anderson@cfwebtools.com>
2021-04-19 15:56:08 -0400

    updated app dependencies and add initial app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {useState} from 'react';
+
 
 const App = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  }
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  }
+
   return (
     <div className="container">
-    
+      <h1>React Dashboard</h1>
     </div>
   );
 }
